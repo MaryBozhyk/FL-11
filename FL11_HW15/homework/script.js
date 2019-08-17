4bc5b51d86325849b1874701e9e0bfc4b142a852
Mariia Bozhyk <46322232+MaryBozhyk@users.noreply.github.com>
2019-08-17 19:47:31 +0300

    Update script.js

--- FL11_HW15/homework/script.js
+++ FL11_HW15/homework/script.js
@@ -23,7 +23,7 @@ class Hamburger{
     }
 
     addCheese(){
-        if(Hamburger.bite > 0) return console.log('Sorry, you cannot add cheese');
+        if(Hamburger.bites > 0) return console.log('Sorry, you cannot add cheese');
         if(this.addCheese.done) return console.log('Sorry, you can add cheese only once');
         this.addCalories(120);
         this.addCheese.done = true;
@@ -31,14 +31,14 @@ class Hamburger{
     }
 
     addTomato(){            
-        if(Hamburger.bite > 0) return console.log('Sorry, you cannot add tomato');
+        if(Hamburger.bites > 0) return console.log('Sorry, you cannot add tomato');
         if(Hamburger.changer === 2) return console.log('Sorry, you can add tomato only twice');
         this.addCalories(20);
         Hamburger.changer += 1;
     }
 
     addSecretIngredient(){
-        if(Hamburger.bite > 0) return console.log('Sorry, you cannot add secret ingredient');
+        if(Hamburger.bites > 0) return console.log('Sorry, you cannot add secret ingredient');
         if(this.addCheese.done || Hamburger.changer) return console.log('Sorry, you can add secret ingredient only before another ingredient');
         if(this.addSecretIngredient.done) return console.log('Sorry, you can add secret ingredient only once');
         this.addCalories(100);
